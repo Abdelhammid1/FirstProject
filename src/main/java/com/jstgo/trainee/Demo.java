5d0c38988f49663159e704524039a21571b1527b
Shalabi <abdelhammid.shalabi@gmail.com>
2023-06-27 00:13:29 +0200

    Add Task Car

--- src/main/java/com/jstgo/trainee/Demo.java
+++ src/main/java/com/jstgo/trainee/Demo.java
@@ -2,12 +2,68 @@ package com.jstgo.trainee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.w3c.dom.DOMImplementation;
 
-@SpringBootApplication
+import java.util.Scanner;
+
+
+//@SpringBootApplication
 public class Demo {
-    public static void main(String[] args) {
-        SpringApplication.run(Demo.class, args);
-        String name = "Eyad"; //test write name to commit and
+
+     static void  AskPeople(){
+         // SpringApplication.run(Demo.class, args);
+         String NameCar, BrandCar, ColorCar, AddCar;
+         Double PriceCar;
+         Scanner WriteWord = new Scanner(System.in);
+         Scanner WriteNumber = new Scanner(System.in);
+         System.out.print("Enter Name Of Car ");  // Output user input
+         NameCar = WriteWord.nextLine();
+
+         System.out.print("Enter Brand ");  // Output user input
+         BrandCar = WriteWord.nextLine();
+
+         System.out.print("Enter Color ");  // Output user input
+         ColorCar = WriteWord.nextLine();
+
+         System.out.print("Enter Price ");  // Output user input
+         PriceCar = Double.valueOf(WriteNumber.nextLine());
+
+         System.out.println("Name of Car is : " + NameCar + ", Brand is " + BrandCar +
+                 ", Color is " + ColorCar + " and price = " + PriceCar + "$");
+
 
     }
+    public static void main(String[] args) {
+         boolean NewCar = true;
+
+         while (NewCar == true){
+             AskPeople();
+             Scanner input = new Scanner(System.in);
+             System.out.print("Do you want add car ");
+             NewCar = Boolean.parseBoolean(input.nextLine());
+         }
+
+
+
+
+//        System.out.print("Enter Name Of Car ");  // Output user input
+//        NameCar = WriteWord.nextLine();
+//
+//        System.out.print("Enter Brand ");  // Output user input
+//        BrandCar = WriteWord.nextLine();
+//
+//        System.out.print("Enter Color ");  // Output user input
+//        ColorCar = WriteWord.nextLine();
+//
+//        System.out.print("Enter Price ");  // Output user input
+//        PriceCar = Double.valueOf(WriteNumber.nextLine());
+//
+//        System.out.println("Name of Car is : " + NameCar + ", Brand is " + BrandCar +
+//                ", Color is " + ColorCar + " and price = " + PriceCar + "$");
+//
+
+    }
+
+
+
 }
