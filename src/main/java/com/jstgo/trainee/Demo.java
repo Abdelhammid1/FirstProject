aab125c2476294bbf906daf821c31b10446223f9
Shalabi <abdelhammid.shalabi@gmail.com>
2023-07-04 00:17:10 +0200

    Add json test file and read it at Consol

--- src/main/java/com/jstgo/trainee/Demo.java
+++ src/main/java/com/jstgo/trainee/Demo.java
@@ -1,60 +1,32 @@
 package com.jstgo.trainee;
 
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.w3c.dom.DOMImplementation;
 
-import java.util.Scanner;
-//@SpringBootApplication
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Demo {
 
-     static void  AskPeople(){
-         // SpringApplication.run(Demo.class, args);
-         String NameCar, BrandCar, ColorCar, AddCar;
-         Double PriceCar;
-         Scanner WriteWord = new Scanner(System.in);
-         Scanner WriteNumber = new Scanner(System.in);
-         System.out.print("Enter Name Of Car ");  // Output user input
-         NameCar = WriteWord.nextLine();
 
-         System.out.print("Enter Brand ");  // Output user input
-         BrandCar = WriteWord.nextLine();
-
-         System.out.print("Enter Color ");  // Output user input
-         ColorCar = WriteWord.nextLine();
-
-         System.out.print("Enter Price ");  // Output user input
-         PriceCar = Double.valueOf(WriteNumber.nextLine());
-
-         System.out.println("Name of Car is : " + NameCar + ", Brand is " + BrandCar +
-                 ", Color is " + ColorCar + " and price = " + PriceCar + "$");
-
-    }
     public static void main(String[] args) {
-         String NewCar = "yes";
+        // Specify the path to your JSON file
+        // Specify the path to your JSON file
+        String filePath = "C:/Projects/FirstProject/test.json";
 
-         while (NewCar.equals("yes")){
-             AskPeople();
-             Scanner input = new Scanner(System.in);
-             System.out.print("Do you want add car ");
-             NewCar = input.nextLine();
-         }
-//        System.out.print("Enter Name Of Car ");  // Output user input
-//        NameCar = WriteWord.nextLine();
-//
-//        System.out.print("Enter Brand ");  // Output user input
-//        BrandCar = WriteWord.nextLine();
-//
-//        System.out.print("Enter Color ");  // Output user input
-//        ColorCar = WriteWord.nextLine();
-//
-//        System.out.print("Enter Price ");  // Output user input
-//        PriceCar = Double.valueOf(WriteNumber.nextLine());
-//
-//        System.out.println("Name of Car is : " + NameCar + ", Brand is " + BrandCar +
-//                ", Color is " + ColorCar + " and price = " + PriceCar + "$");
-//
+        try {
+            // Read the file contents as a string
+            String jsonContent = new String(Files.readAllBytes(Paths.get(filePath)));
 
+            // Print the JSON content
+            System.out.println("JSON Content:");
+            System.out.println(jsonContent);
+
+            // Parse the JSON content if needed
+            // ...
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
 
 }
