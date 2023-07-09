6f29ce10f7979f03625e6bdad876525d1c02c390
Shalabi <abdelhammid.shalabi@gmail.com>
2023-07-09 11:57:52 +0200

    Finished this task

--- src/main/java/com/jstgo/trainee/Demo.java
+++ src/main/java/com/jstgo/trainee/Demo.java
@@ -1,32 +1,52 @@
 package com.jstgo.trainee;
 
 
+
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class Demo {
 
 
-    public static void main(String[] args) {
-        // Specify the path to your JSON file
-        // Specify the path to your JSON file
-        String filePath = "C:/Projects/FirstProject/test.json";
+    public static void main(String[] args) throws IOException {
+        float NumOfSubj;
+        float MarksOfSubj;
+        float SumOfMarks=0;
+        float AvgOfMarks;
+        char  FinalGrade;
 
-        try {
-            // Read the file contents as a string
-            String jsonContent = new String(Files.readAllBytes(Paths.get(filePath)));
 
-            // Print the JSON content
-            System.out.println("JSON Content:");
-            System.out.println(jsonContent);
-
-            // Parse the JSON content if needed
-            // ...
-        } catch (IOException e) {
-            e.printStackTrace();
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Number Of Subject: ");
+        NumOfSubj = input.nextFloat();
+        for (int i=1; i<=NumOfSubj; i++){
+            System.out.print("Enter Marks of Subject "+i+": ");
+            Scanner inputMark = new Scanner(System.in);
+            MarksOfSubj = inputMark.nextFloat();
+            SumOfMarks += MarksOfSubj;
         }
+
+        AvgOfMarks =  SumOfMarks / NumOfSubj;
+        if (AvgOfMarks >=90 ){
+        FinalGrade = 'A';
+        }
+        else if (AvgOfMarks>=80 && AvgOfMarks<90){
+        FinalGrade = 'B';
+        }
+        else if (AvgOfMarks>=70 && AvgOfMarks<80){
+            FinalGrade = 'C';
+        }
+        else if (AvgOfMarks>=60 && AvgOfMarks<70){
+            FinalGrade = 'D';
+        }
+        else {
+            FinalGrade = 'F';
+
+        }
+        System.out.println("Average Marks: " + AvgOfMarks );
+
+        System.out.println("Final Grade: " +FinalGrade );
+
+
     }
-
-
 }
