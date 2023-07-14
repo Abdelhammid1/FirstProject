d3e94424a1f1d5d9daed3b635f699b5fe8bd6733
Shalabi <abdelhammid.shalabi@gmail.com>
2023-07-14 23:40:04 +0200

    test

--- src/main/java/com/jstgo/trainee/Demo.java
+++ src/main/java/com/jstgo/trainee/Demo.java
@@ -1,9 +1,13 @@
 package com.jstgo.trainee;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
-import com.jstgo.trainee.vo.GsonRoot;
-import org.json.simple.JSONObject;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
@@ -11,34 +15,13 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class Demo {
-    public static void main(String[] args) throws IOException {
-        String filePath = "C:/Projects/FirstProject/test.json";   // Specify the path to your JSON file
+    public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {
+        File xmlFile = new File("C:/Projects/FirstProject/test.xml");
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(xmlFile);
+        doc.getDocumentElement();
 
-        String jsonData = new String(Files.readAllBytes(Paths.get(filePath)));
-
-        //Parse JSON from string
-        JsonObject jsonObject = new Gson().fromJson(jsonData, JsonObject.class);
-        System.out.println("Original JSON: " + jsonObject);
-
-
-//        System.out.println("Original JSON: " + jsonObject);
-        jsonObject.addProperty("Age", "25");
-        jsonObject.addProperty("New Line","Added");
-        System.out.println("Modified JSON: " + jsonObject);
-        try(Writer writerObject = new FileWriter(filePath)){
-
-            Gson gsonObject = new Gson();
-
-            /*
-             * Use the toJson method and specify
-             * the writer and the object we want to write
-             */
-            gsonObject.toJson(jsonObject, writerObject);
-
-        }catch(Exception e) {
-            e.printStackTrace();
-        }
-        System.out.println("Original JSON: " + jsonObject);
 
 
     }
